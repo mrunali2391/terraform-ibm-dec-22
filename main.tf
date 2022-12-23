ab2ab60303f157090f64e231985809fea37053a9
mrunali2391 <60960139+mrunali2391@users.noreply.github.com>
2022-12-23 13:05:09 +0530

    commit

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
 provider "aws" {
     region = "us-east-1"
 }
+
+resource "tls_private_key" "private_key" {
+  algorithm = "RSA"
+  rsa_bits = "2048"
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name = var.name
+  public_key = tls_private_key.private_key.public_key_openssh
+  tags = {
+    Name = var.name
+  }
+
+}
+
